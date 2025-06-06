3550ed45b292e6ab221a14ec4b8641b281b59878
YRMLunar <198003617@qq.com>
2025-06-06 15:32:58 +0800

    组织祈福和追击晓组织

---
+++ agent/restart/reco.py
@@ -0,0 +1,12 @@
+from maa.agent.agent_server import AgentServer
+from maa.context import Context
+from maa.custom_recognition import CustomRecognition
+
+
+@AgentServer.custom_recognition("Naruto_start")
+class reco:
+    def analyze(self):
+
+
+
+
