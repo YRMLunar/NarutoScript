53409d89054cdb58ee96a1076b8a78bfcb36e576
YRMLunar <198003617@qq.com>
2025-07-31 22:59:09 +0800

    组织祈福

---
+++ tasks/daily/assets/assets_daily.py
@@ -0,0 +1,203 @@
+from module.base.button import Button, ButtonWrapper
+
+# This file was auto-generated, do not modify it manually. To generate:
+# ``` python -m dev_tools.button_extract ```
+
+DAILY_BAR = ButtonWrapper(
+    name='DAILY_BAR',
+    cn=Button(
+        file='./assets/cn/daily/DAILY_BAR.png',
+        area=(429, 539, 1148, 598),
+        search=(409, 519, 1168, 618),
+        color=(94, 77, 55),
+        button=(429, 539, 1148, 598),
+    ),
+    en=None,
+)
+DAILY_CHECK = ButtonWrapper(
+    name='DAILY_CHECK',
+    cn=Button(
+        file='./assets/cn/daily/DAILY_CHECK.png',
+        area=(1, 0, 267, 125),
+        search=(0, 0, 287, 145),
+        color=(146, 109, 43),
+        button=(1, 0, 267, 125),
+    ),
+    en=None,
+)
+DAILY_EXIT = ButtonWrapper(
+    name='DAILY_EXIT',
+    cn=Button(
+        file='./assets/cn/daily/DAILY_EXIT.png',
+        area=(1170, 0, 1280, 77),
+        search=(1150, 0, 1280, 97),
+        color=(62, 37, 38),
+        button=(1170, 0, 1280, 77),
+    ),
+    en=None,
+)
+DAILY_PROGRESS = ButtonWrapper(
+    name='DAILY_PROGRESS',
+    cn=Button(
+        file='./assets/cn/daily/DAILY_PROGRESS.png',
+        area=(360, 548, 410, 593),
+        search=(340, 528, 430, 613),
+        color=(94, 86, 49),
+        button=(360, 548, 410, 593),
+    ),
+    en=None,
+)
+DAILY_REWARD_10 = ButtonWrapper(
+    name='DAILY_REWARD_10',
+    cn=Button(
+        file='./assets/cn/daily/DAILY_REWARD_10.png',
+        area=(474, 526, 551, 595),
+        search=(454, 506, 571, 615),
+        color=(141, 120, 35),
+        button=(474, 526, 551, 595),
+    ),
+    en=None,
+)
+DAILY_REWARD_100 = ButtonWrapper(
+    name='DAILY_REWARD_100',
+    cn=Button(
+        file='./assets/cn/daily/DAILY_REWARD_100.png',
+        area=(1137, 523, 1233, 593),
+        search=(1117, 503, 1253, 613),
+        color=(122, 96, 52),
+        button=(1137, 523, 1233, 593),
+    ),
+    en=None,
+)
+DAILY_REWARD_100_DONE = ButtonWrapper(
+    name='DAILY_REWARD_100_DONE',
+    cn=Button(
+        file='./assets/cn/daily/DAILY_REWARD_100_DONE.png',
+        area=(1118, 501, 1239, 602),
+        search=(1098, 481, 1259, 622),
+        color=(142, 109, 54),
+        button=(1118, 501, 1239, 602),
+    ),
+    en=None,
+)
+DAILY_REWARD_10_DONE = ButtonWrapper(
+    name='DAILY_REWARD_10_DONE',
+    cn=Button(
+        file='./assets/cn/daily/DAILY_REWARD_10_DONE.png',
+        area=(454, 494, 569, 599),
+        search=(434, 474, 589, 619),
+        color=(113, 99, 55),
+        button=(454, 494, 569, 599),
+    ),
+    en=None,
+)
+DAILY_REWARD_40 = ButtonWrapper(
+    name='DAILY_REWARD_40',
+    cn=Button(
+        file='./assets/cn/daily/DAILY_REWARD_40.png',
+        area=(697, 524, 774, 596),
+        search=(677, 504, 794, 616),
+        color=(72, 92, 95),
+        button=(697, 524, 774, 596),
+    ),
+    en=None,
+)
+DAILY_REWARD_40_DONE = ButtonWrapper(
+    name='DAILY_REWARD_40_DONE',
+    cn=Button(
+        file='./assets/cn/daily/DAILY_REWARD_40_DONE.png',
+        area=(669, 499, 805, 594),
+        search=(649, 479, 825, 614),
+        color=(110, 117, 98),
+        button=(669, 499, 805, 594),
+    ),
+    en=None,
+)
+DAILY_REWARD_80 = ButtonWrapper(
+    name='DAILY_REWARD_80',
+    cn=Button(
+        file='./assets/cn/daily/DAILY_REWARD_80.png',
+        area=(991, 516, 1070, 590),
+        search=(971, 496, 1090, 610),
+        color=(119, 90, 63),
+        button=(991, 516, 1070, 590),
+    ),
+    en=None,
+)
+DAILY_REWARD_80_DONE = ButtonWrapper(
+    name='DAILY_REWARD_80_DONE',
+    cn=Button(
+        file='./assets/cn/daily/DAILY_REWARD_80_DONE.png',
+        area=(955, 500, 1091, 596),
+        search=(935, 480, 1111, 616),
+        color=(124, 99, 69),
+        button=(955, 500, 1091, 596),
+    ),
+    en=None,
+)
+MAIN_GOTO_DAILY = ButtonWrapper(
+    name='MAIN_GOTO_DAILY',
+    cn=Button(
+        file='./assets/cn/daily/MAIN_GOTO_DAILY.png',
+        area=(1189, 320, 1272, 392),
+        search=(1169, 300, 1280, 412),
+        color=(85, 77, 70),
+        button=(1189, 320, 1272, 392),
+    ),
+    en=None,
+)
+WEEKLY_CHECK = ButtonWrapper(
+    name='WEEKLY_CHECK',
+    cn=Button(
+        file='./assets/cn/daily/WEEKLY_CHECK.png',
+        area=(539, 191, 760, 247),
+        search=(519, 171, 780, 267),
+        color=(51, 90, 116),
+        button=(539, 191, 760, 247),
+    ),
+    en=None,
+)
+WEEKLY_CLAIM = ButtonWrapper(
+    name='WEEKLY_CLAIM',
+    cn=Button(
+        file='./assets/cn/daily/WEEKLY_CLAIM.png',
+        area=(541, 423, 740, 495),
+        search=(521, 403, 760, 515),
+        color=(213, 142, 14),
+        button=(541, 423, 740, 495),
+    ),
+    en=None,
+)
+WEEKLY_CLAIM_DONE = ButtonWrapper(
+    name='WEEKLY_CLAIM_DONE',
+    cn=Button(
+        file='./assets/cn/daily/WEEKLY_CLAIM_DONE.png',
+        area=(548, 401, 762, 507),
+        search=(528, 381, 782, 527),
+        color=(228, 212, 186),
+        button=(548, 401, 762, 507),
+    ),
+    en=None,
+)
+WEEKLY_EXIT = ButtonWrapper(
+    name='WEEKLY_EXIT',
+    cn=Button(
+        file='./assets/cn/daily/WEEKLY_EXIT.png',
+        area=(837, 191, 910, 249),
+        search=(817, 171, 930, 269),
+        color=(83, 45, 42),
+        button=(837, 191, 910, 249),
+    ),
+    en=None,
+)
+WEEKlY_BUTTON = ButtonWrapper(
+    name='WEEKlY_BUTTON',
+    cn=Button(
+        file='./assets/cn/daily/WEEKlY_BUTTON.png',
+        area=(1194, 655, 1267, 706),
+        search=(1174, 635, 1280, 720),
+        color=(76, 70, 48),
+        button=(1194, 655, 1267, 706),
+    ),
+    en=None,
+)
